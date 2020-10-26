3d1ea81afc79c874368390c484fa527f244d840f
deepcode <13121327906bxy@gmail.com>
2020-10-26 14:21:55 +0800

    add android upgrade functions

--- README.md
+++ README.md
@@ -8,6 +8,13 @@
 npm install dianvo-native-bugly
 ```
 
+ implementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
+  //    implementation 'com.tencent.bugly:crashreport:latest.release'
+  //其中latest.release指代最新Bugly SDK版本号，也可以指定明确的版本号，例如2.1.9
+  api 'com.tencent.bugly:crashreport_upgrade:latest.release'
+//其中latest.release指代最新版本号，也可以指定明确的版本号，例如1.2.0
+  api 'com.tencent.bugly:nativecrashreport:latest.release'
+
 ## Usage
 
 ```js
@@ -17,6 +24,25 @@ import DianvoNativeBugly from "dianvo-native-bugly";
 
 const result = await DianvoNativeBugly.multiply(3, 7);
 ```
+```
+import SimpleRnBugly from './simple-rn-bugly/src/index'
+  <Button title={"Get Update information"} onPress={async () => {
+            let updateInfo = await SimpleRnBugly.getUpgradeInfo();
+            console.log("update info : "+updateInfo.versionCode);
+  }}/>
+  <Button title={"app Update"} onPress={async () => {
+            SimpleRnBugly.checkAppUpgrade({isManual:true,
+                isSilence:false});
+  }}/>
+```
+
+## Note
+if you use android 9.0 and above ,please set following statement in your < application > tag
+```css
+android:usesCleartextTraffic="true"
+```
+or use custom network security config
+
 
 ## Contributing
 
--- android/src/main/java/com/dianvonativebugly/DianvoNativeBuglyModule.kt
+++ android/src/main/java/com/dianvonativebugly/DianvoNativeBuglyModule.kt
@@ -4,6 +4,64 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import android.content.Context
+import android.os.Process
+import android.text.TextUtils
+import com.facebook.react.bridge.*
+import com.tencent.bugly.Bugly
+import com.tencent.bugly.beta.Beta
+import com.tencent.bugly.crashreport.BuglyLog
+import com.tencent.bugly.crashreport.CrashReport
+import com.tencent.bugly.crashreport.CrashReport.UserStrategy
+import java.io.BufferedReader
+import java.io.FileReader
+import java.io.IOException
+import java.util.*
+
+/**
+ * 使用了bugly的多进程策略初始化bugly
+ * 避免了多进程的时候重复上报异常
+ */
+fun initBuglyWithStrategy(context: Context,appId:String,isDebug:Boolean) {
+  // 获取当前包名
+  val packageName = context.packageName
+  // 获取当前进程名
+  val processName: String? = getProcessName(Process.myPid())
+  // 设置是否为上报进程
+  val strategy = UserStrategy(context)
+  strategy.isUploadProcess = processName == null || processName == packageName
+  //upgrade 包中已经集成了crash report
+  //不自动检查更新也不自动初始化，做成包提供给rn调用
+  Beta.autoCheckUpgrade = false
+  Bugly.init(context, appId,isDebug)
+}
+
+/**
+ * 获取进程号对应的进程名
+ *
+ * @param pid 进程号
+ * @return 进程名
+ */
+fun getProcessName(pid: Int): String? {
+  var reader: BufferedReader? = null
+  try {
+    reader = BufferedReader(FileReader("/proc/$pid/cmdline"))
+    var processName = reader.readLine()
+    if (!TextUtils.isEmpty(processName)) {
+      processName = processName.trim { it <= ' ' }
+    }
+    return processName
+  } catch (throwable: Throwable) {
+    throwable.printStackTrace()
+  } finally {
+    try {
+      reader?.close()
+    } catch (exception: IOException) {
+      exception.printStackTrace()
+    }
+  }
+  return null
+}
 
 class DianvoNativeBuglyModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
@@ -11,14 +69,50 @@ class DianvoNativeBuglyModule(reactContext: ReactApplicationContext) : ReactCont
         return "DianvoNativeBugly"
     }
 
-    // Example method
-    // See https://facebook.github.io/react-native/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
-    }
 
-    
+  @ReactMethod
+  fun checkAppUpgrade(options: ReadableMap) {
+    //用户手动点击检查，非用户点击操作请传false
+    var isManual = true
+    //是否显示弹窗等交互，[true:没有弹窗和toast] [false:有弹窗或toast]
+    var isSilence = false
+    if (options.hasKey("isManual")) {
+      isManual = options.getBoolean("isManual")
+    }
+    if (options.hasKey("isSilence")) {
+      isSilence = options.getBoolean("isSilence")
+    }
+    Beta.checkAppUpgrade(isManual, isSilence)
+  }
+
+
+  @ReactMethod
+  fun getUpgradeInfo(promise: Promise) {
+    val info = Beta.getAppUpgradeInfo()
+    if (info != null) {
+      val writableMap = Arguments.createMap()
+      writableMap.putString("apkMd5", info.apkMd5)
+      writableMap.putString("apkUrl", info.apkUrl)
+      writableMap.putString("id", info.id)
+      writableMap.putString("imageUrl", info.imageUrl)
+      writableMap.putString("newFeature", info.newFeature)
+      writableMap.putString("title", info.title)
+      writableMap.putString("versionName", info.versionName)
+      writableMap.putDouble("fileSize", info.fileSize.toDouble())
+      //弹窗间隔（ms）
+      writableMap.putDouble("popInterval", info.popInterval.toDouble())
+      //弹窗次数:
+      writableMap.putInt("popTimes", info.popTimes)
+      //发布类型（0:测试 1:正式）:
+      writableMap.putInt("publishType", info.publishType)
+      writableMap.putInt("updateType", info.updateType)
+      //弹窗类型（1:建议 2:强制 3:手工）:
+      writableMap.putInt("upgradeType", info.upgradeType)
+      writableMap.putInt("versionCode", info.versionCode)
+      writableMap.putDouble("publishTime", info.publishTime.toDouble())
+      promise.resolve(writableMap)
+    }
+    promise.resolve(null)
+  }
+
 }
